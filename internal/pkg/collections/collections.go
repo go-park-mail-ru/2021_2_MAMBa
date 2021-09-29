6d5d8a45d8127c781ee6e3df972bd654a5bdfe6e
Alexey Samoylov <trollex2013@gmail.com>
2021-09-29 17:25:04 +0300

    Исправил опечатки в моке коллекций

--- internal/pkg/collections/collections.go
+++ internal/pkg/collections/collections.go
@@ -24,7 +24,7 @@ type collections struct {
 }
 
 var previewMock = []collectionPreview{
-	{Id: 1, Title: "Для ценителей Хогвардса", PictureUrl: "server/images/collections1.png"},
+	{Id: 1, Title: "Для ценителей Хогвартса", PictureUrl: "server/images/collections1.png"},
 	{Id: 2, Title: "Про настоящую любовь", PictureUrl: "server/images/collections2.png"},
 	{Id: 3, Title: "Аферы века", PictureUrl: "server/images/collections3.png"},
 	{Id: 4, Title: "Про Вторую Мировую", PictureUrl: "server/images/collections4.jpg"},
@@ -32,7 +32,7 @@ var previewMock = []collectionPreview{
 	{Id: 6, Title: "Летняя атмосфера", PictureUrl: "server/images/collections6.png"},
 	{Id: 7, Title: "Про дружбу", PictureUrl: "server/images/collections7.png"},
 	{Id: 8, Title: "Романтические фильмы", PictureUrl: "server/images/collections8.jpg"},
-	{Id: 9, Title: "Джунгди зовут", PictureUrl: "server/images/collections9.jpg"},
+	{Id: 9, Title: "Джунгли зовут", PictureUrl: "server/images/collections9.jpg"},
 	{Id: 10, Title: "Фантастические фильмы", PictureUrl: "server/images/collections10.jpg"},
 	{Id: 11, Title: "Про петлю времени", PictureUrl: "server/images/collections11.png"},
 	{Id: 12, Title: "Классика на века", PictureUrl: "server/images/collections12.jpg"},
